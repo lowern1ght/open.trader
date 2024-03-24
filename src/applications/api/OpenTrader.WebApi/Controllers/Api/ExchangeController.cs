e7ffaebdbd6f1079561c9f95612c47b57ffed94b
Alexander Sergeev | lowern1ght <lowern1ght@yahoo.com>
2024-03-24 23:34:00 +0500

    [identity-v2](feature): description:
    1. fix bugs in identity, configure cookie options
    2. add xml documents for all endpoints
    3. Recode ExchangeController.cs
    
    #other
    
    * fix nginx config

--- src/applications/api/OpenTrader.WebApi/Controllers/Api/ExchangeController.cs
+++ src/applications/api/OpenTrader.WebApi/Controllers/Api/ExchangeController.cs
@@ -1,33 +1,49 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cors;
+﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using OpenTrader.Constants.General;
+using Microsoft.AspNetCore.Authorization;
 using OpenTrader.Exchange.Service.Interfaces;
 
 namespace OpenTrader.WebApi.Controllers.Api;
 
+/// <summary> Controller work with exchanges </summary>
+/// <param name="logger"></param>
+/// <param name="imageService"></param>
+/// <param name="exchangeService"></param>
 [Authorize]
 [ApiController]
 [Route("~/api/v1/[controller]/")]
 [EnableCors(CorsPolicies.AllowAll)]
-public class ExchangeController(IExchangeService exchangeService, IExchangeImageService imageService) : Controller
+public class ExchangeController(ILogger<ExchangeController> logger, IExchangeService exchangeService, IExchangeImageService imageService) : Controller
 {
-    [HttpGet]
-    public async Task<IActionResult> GetAllAsync(CancellationToken token)
+    /// <summary> </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    [Authorize, HttpGet]
+    [ResponseCache(Duration = 1200, Location = ResponseCacheLocation.Any)]
+    public async Task<IActionResult> ListAsync(CancellationToken token)
     {
         return Ok((await exchangeService.CollectionAsync(token)).ToArray());
     }
     
-    [HttpGet("{name}/image")]
-    public async Task GetImageById(string name, CancellationToken token)
+    /// <summary> Return svg image from s3 </summary>
+    /// <param name="name"></param>
+    /// <param name="token"></param>
+    /// <response code="200">Return <c>ClaimIdentity</c> from <c>HttpContext</c>></response>
+    /// <response code="500">Unhandled exception</response>
+    [Authorize, HttpGet("{name}/image")]
+    [ResponseCache(Duration = 3600, Location = ResponseCacheLocation.Any)]
+    public async Task<IActionResult> ImageAsync(string name, CancellationToken token)
     {
         try
         {
-            await imageService.DownloadImageByName(name, token);
+            var imageResult = await imageService.DownloadImageByName(name, token);
+            return File(imageResult.FileStream, imageResult.ContentType);
         }
-        catch (Exception)
+        catch (Exception exception)
         {
-            Response.StatusCode = StatusCodes.Status500InternalServerError;
+            logger.Log(LogLevel.Error, "{Exception}", exception.Message);
+            return Problem(exception.Message);
         }
     }
 }
